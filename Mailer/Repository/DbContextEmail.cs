4d7719ec1f7db124dd8049233aeb5592799c0420
Damir Safarov <damup1988@gmail.com>
2022-02-20 18:26:18 +0300

    Email Db context addedd

---
+++ Mailer/Repository/DbContextEmail.cs
@@ -0,0 +1,12 @@
+﻿using Mailer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mailer.Repository
+{
+    public class DbContextEmail : DbContext
+    {
+        public DbContextEmail(DbContextOptions<DbContextEmail> opt) : base(opt) {}
+
+        public DbSet<Email> Emails { get; set; }
+    }
+}
